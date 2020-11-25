079ba991bb5a66110b88e0bc69f4be15c476fd24
Osef1712 <j.zumkir@ludus-academie.com>
2020-11-25 15:33:33 +0100

    Question7
    Comment créer une fenêtre avec SDL 2, donnez et expliquez le code correspondant à la création d’une fenêtre avec SDL 2.

--- SLD_WhatIsThat/main.c
+++ SLD_WhatIsThat/main.c
@@ -2,7 +2,10 @@
 #include <stdlib.h>
 #include <SDL2/SDL.h>
 
+#define WINDOW_WIDTH 1024
+#define WINDOW_HEIGHT 780
 
+SDL_Window *pWindow=NULL;
 
 int main(int argc, char *argv[])
 {   //Vérification si l'initialisation s'est mal passé
@@ -13,6 +16,17 @@ int main(int argc, char *argv[])
         SDL_Quit();
         //Sortir du programme
         return 1;
+    }else{
+        //Création d'une fenêtre
+        pWindow=SDL_CreateWindow("An SDL2 window",200,100,WINDOW_WIDTH,WINDOW_HEIGHT,SDL_WINDOW_SHOWN);
+    }
+    //Permet de voir le rendu de la page
+    SDL_Delay(1000);
+
+    //Vérification si il y a bien une fenêtre
+    if(pWindow){
+        //Détruire la fenêtre
+        SDL_DestroyWindow(pWindow);
     }
     return 0;
 }
